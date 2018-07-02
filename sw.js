e0d235a35d34282abddb7a5890ba94e6e3c4094d
Dani <danihazler@gmail.com>
2018-07-02 17:25:20 +0100

    Add respondWith, part 1

--- sw.js
+++ sw.js
@@ -1,9 +1,44 @@
+let cacheName = 'cache_v3';
+let cacheFiles = [
+  './',
+  'index.html',
+  '.css/styles.css',
+  '.css/responsive.css',
+  'img/Casa_Enrique.jpg',
+  'img/Emily.jpg',
+  '.js/dbhelper.js',
+  './js/restaurant_info.js',
+  './data/restaurants.json',
+  'https://fonts.googleapis.com/css?family=Barlow+Condensed:300,400,500,600',
+  'https://fonts.googleapis.com/css?family=Montserrat:600'
+];
+
 self.addEventListener('install', function(e){
-  console.log("[ServiceWorker] Installed")
-})
+  e.waitUntil(caches.open(cacheName).then(cache => {
+    return cache.addAll(cacheFiles);
+  }).catch(err => {
+    console.log("Cache has not been added.");
+  }));
+});
+
 self.addEventListener('activate', function(e){
-  console.log("[ServiceWorker] Active")
-})
+  e.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(cacheNames.map(thisCacheName => {
+        if (thisCacheName !== cacheName) {
+          return caches.delete(thisCacheName);
+        }
+      }))
+    }).catch(err => {
+      console.log("Old cache was not deleted.");
+    })
+  );
+});
+
 self.addEventListener('fetch', function(e){
-  console.log("[ServiceWorker] Fetching", e.request.url)
-})
+  e.respondWith(
+    caches.match(e.resquest)
+    .then(response => {
+      return response || fetch(e.request);
+  }));
+});
